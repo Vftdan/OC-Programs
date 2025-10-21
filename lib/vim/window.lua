2023e1db1b509931ebd7482d0817c235396d78e3
Vftdan <vftdan@gmail.com>
2025-10-21 02:12:25 +0200

    Basic tabulation rendering support
    Outside of the rendering code, it is still considered to be a single column, causing cursor jumps and weird scrolling

--- lib/vim/window.lua
+++ lib/vim/window.lua
@@ -2,6 +2,170 @@ local textrender = require("vim.platform.textrender")
 local sysencoding = require("vim.platform.sysencoding")
 local itertools = require("vim.itertools")
 local makeClass = require("vim.makeclass")
+local strptn = require("vim.strptn")
+
+local TABSTOP = 8
+
+local function toPrintableStyled(line)
+	-- TODO non-printable
+	local tabPositions = strptn.findAll(line, "\t")
+	local result = {}
+	local adjustment = 0
+	local start = 1
+	local lineLen = sysencoding.len(line)
+	for _, m in ipairs(tabPositions) do
+		if start < m[1] then
+			table.insert(result, {
+				string = sysencoding.sub(line, start, m[1] - 1),
+				styleNames = {"normal"},
+				firstCodePoint = start,
+				lastCodePoint = m[1] - 1,
+				firstColumn = start + adjustment,
+				lastColumn = m[1] - 1 + adjustment,
+			})
+		end
+		local startCol = m[1] + adjustment
+		local width = TABSTOP - ((startCol - 1) % TABSTOP)
+		table.insert(result, {
+			string = itertools.repeatString("-", width - 1) .. ">",
+			styleNames = {"normal", "nontext"},
+			firstCodePoint = m[1],
+			lastCodePoint = m[2],
+			firstColumn = m[1] + adjustment,
+			lastColumn = m[1] + adjustment + width - 1,
+		})
+		adjustment = adjustment + width - 1
+		start = m[2] + 1
+	end
+	if start <= lineLen then
+		table.insert(result, {
+			string = sysencoding.sub(line, start),
+			styleNames = {"normal"},
+			firstCodePoint = start,
+			lastCodePoint = lineLen,
+			firstColumn = start + adjustment,
+			lastColumn = lineLen + adjustment,
+		})
+	end
+	return result
+end
+
+local function printableStyledToView(orig, scrollX, contentWidth)
+	local result = {}
+	local firstCol = scrollX + 1
+	local lastCol = scrollX + contentWidth
+	for _, entry in ipairs(orig) do
+		if entry.lastColumn >= firstCol and entry.firstColumn <= lastCol then
+			if entry.lastColumn > lastCol then
+				local s = sysencoding.sub(entry.string, 1, lastCol - entry.firstColumn + 1)
+				entry = {
+					string = s,
+					styleNames = entry.styleNames,
+					firstCodePoint = entry.firstCodePoint,
+					lastCodePoint = entry.lastCodePoint,  -- Do not update
+					firstColumn = entry.firstColumn,
+					lastColumn = lastCol,
+				}
+			end
+			if entry.firstColumn < firstCol then
+				local amount = firstCol - entry.firstColumn
+				local s = sysencoding.sub(entry.string, amount + 1)
+				entry = {
+					string = s,
+					styleNames = entry.styleNames,
+					firstCodePoint = entry.firstCodePoint + amount,
+					lastCodePoint = entry.lastCodePoint,
+					firstColumn = entry.firstColumn,
+					lastColumn = lastCol,
+				}
+				if entry.lastCodePoint < entry.firstCodePoint then
+					entry.firstCodePoint = entry.lastCodePoint
+				end
+			end
+			table.insert(result, entry)
+		end
+	end
+	local lastPresentCol = 0
+	local lastPresentCodePoint = 0
+	if #result > 0 then
+		lastPresentCol = result[#result].lastColumn
+		lastPresentCodePoint = result[#result].lastCodePoint
+	end
+	local rightPadLength = lastCol - lastPresentCol
+	if rightPadLength > 0 then
+		local rightPad = itertools.repeatString(" ", rightPadLength)
+		local entry = {
+			string = rightPad,
+			styleNames = {"normal"},
+			firstCodePoint = lastPresentCodePoint + 1,
+			lastCodePoint = lastPresentCodePoint + rightPadLength,
+			firstColumn = lastPresentCol,
+			lastColumn = lastCol,
+		}
+		table.insert(result, entry)
+	end
+	return result
+end
+
+local function printableStyledAddCursor(orig, cursorX)
+	local result = {}
+	for _, entry in ipairs(orig) do
+		if cursorX < entry.firstCodePoint or cursorX > entry.lastCodePoint then
+			table.insert(result, entry)
+		else
+			local x = cursorX - entry.firstCodePoint + 1
+			if x > 1 then
+				local s = sysencoding.sub(entry.string, 1, x - 1)
+				local newEntry = {
+					string = s,
+					styleNames = entry.styleNames,
+					firstCodePoint = entry.firstCodePoint,
+					lastCodePoint = entry.firstCodePoint + x - 2,
+					firstColumn = entry.firstColumn,
+					lastColumn = entry.firstColumn + x - 2,
+				}
+				if newEntry.lastCodePoint > entry.lastCodePoint then
+					newEntry.lastCodePoint = entry.lastCodePoint
+				end
+				table.insert(result, newEntry)
+			end
+			local s = sysencoding.sub(entry.string, x, x)
+			local styleWithCursor = itertools.collect(ipairs(entry.styleNames))
+			styleWithCursor[#styleWithCursor + 1] = "cursor"
+			local newEntry = {
+				string = s,
+				styleNames = styleWithCursor,
+				firstCodePoint = entry.firstCodePoint + x - 1,
+				lastCodePoint = entry.firstCodePoint + x - 1,
+				firstColumn = entry.firstColumn + x - 1,  -- = cursorX
+				lastColumn = entry.firstColumn + x - 1,
+			}
+			if newEntry.firstCodePoint > entry.lastCodePoint then
+				newEntry.firstCodePoint = entry.lastCodePoint
+			end
+			if newEntry.lastCodePoint > entry.lastCodePoint then
+				newEntry.lastCodePoint = entry.lastCodePoint
+			end
+			table.insert(result, newEntry)
+			if x < sysencoding.len(entry.string) then
+				local s = sysencoding.sub(entry.string, x + 1)
+				local newEntry = {
+					string = s,
+					styleNames = entry.styleNames,
+					firstCodePoint = entry.firstCodePoint + x,
+					lastCodePoint = entry.lastCodePoint,
+					firstColumn = entry.firstColumn + x,
+					lastColumn = entry.lastColumn,
+				}
+				if newEntry.firstCodePoint > entry.lastCodePoint then
+					newEntry.firstCodePoint = entry.lastCodePoint
+				end
+				table.insert(result, newEntry)
+			end
+		end
+	end
+	return result
+end
 
 local Window = makeClass {
 	init = function(self, buffer)
@@ -41,44 +205,22 @@ local Window = makeClass {
 				end
 				local nrChunk = {nrString .. " "}
 				itertools.update(nrChunk, lineNrStyle)
-				local viewportLine = sysencoding.sub(self.currentBuffer:getLine(lineNr), 1 + self._scrollX, contentWidth + self._scrollX)
+				local blitData = {nrChunk}
+
+				local psl = self:_getPrintableStyledLine(lineNr)
+				psl = printableStyledToView(psl, self._scrollX, contentWidth)
 				if cursorViewportY == i and cursorViewportX > 0 then
-					local linePre = sysencoding.sub(viewportLine, 1, cursorViewportX - 1)
-					local lineCur = sysencoding.sub(viewportLine, cursorViewportX, cursorViewportX)
-					if lineCur == "" then
-						lineCur = " "
-					end
-					local linePost = sysencoding.sub(viewportLine, cursorViewportX + 1)
-					local remainingLength = contentWidth - sysencoding.len(linePre) - sysencoding.len(linePost) - 1
-					local rightPad = ""
-					if remainingLength > 0 then
-						rightPad = itertools.repeatString(" ", remainingLength)
-					end
-					local chunkPre = {linePre}
-					local chunkCur = {lineCur}
-					local chunkPost = {linePost .. rightPad}
-					itertools.update(chunkPre, normalStyle)
-					itertools.update(chunkCur, cursorStyle)
-					itertools.update(chunkPost, normalStyle)
-					textrender.blitAll{
-						nrChunk,
-						chunkPre,
-						chunkCur,
-						chunkPost,
-					}
-				else
-					local remainingLength = contentWidth - sysencoding.len(viewportLine)
-					local rightPad = ""
-					if remainingLength > 0 then
-						rightPad = itertools.repeatString(" ", remainingLength)
-					end
-					local textChunk = {viewportLine .. rightPad}
-					itertools.update(textChunk, normalStyle)
-					textrender.blitAll{
-						nrChunk,
-						textChunk,
-					}
+					psl = printableStyledAddCursor(psl, cursorViewportX)
 				end
+				for _, entry in ipairs(psl) do
+					local chunk = {entry.string}
+					-- TODO cache
+					local style = textrender.interpretStyle(self._editor.styleRegistry:resolveStack(entry.styleNames))
+					itertools.update(chunk, style)
+					table.insert(blitData, chunk)
+				end
+
+				textrender.blitAll(blitData)
 			else
 				textrender.blitAll{
 					eobChunk,
@@ -119,6 +261,18 @@ local Window = makeClass {
 	setScrollAmount = function(self, x, y)
 		self._scrollX = x
 		self._scrollY = y
+	end,
+	_getPrintableStyledLine = function(self, y)
+		local result = self.currentBuffer:getScopedLineCache(y, "printableStyled")
+		if result == nil then
+			local line = self.currentBuffer:getLine(y)
+			if line == nil then
+				return nil
+			end
+			result = toPrintableStyled(line)
+			self.currentBuffer:setScopedLineCache(y, "printableStyled", result)
+		end
+		return result
 	end,
 }
 
