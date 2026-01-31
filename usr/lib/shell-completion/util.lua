e4fef1a65937f6042ebc256d65f7fa98b5401767
Vftdan <vftdan@gmail.com>
2026-01-31 21:16:35 +0100

    Utility function to complete flags

--- usr/lib/shell-completion/util.lua
+++ usr/lib/shell-completion/util.lua
@@ -10,7 +10,36 @@ local function populateMatching(result, prefix, arr)
   end
 end
 
+local function populateOptions(result, argvOrPrefix, shortOptions, longOptions)
+  local prefix
+  if type(argvOrPrefix) == "string" then
+    prefix = argvOrPrefix
+  else
+    prefix = argvOrPrefix[#argvOrPrefix]
+  end
+  if prefix:sub(1, 1) ~= "-" then
+    -- Do not show options if not requested
+    return
+  end
+  local arr = {}
+  if prefix:sub(1, 2) == "--" and type(longOptions) == "table" then
+    for _, name in ipairs(longOptions) do
+      local option = "--" .. name
+      if not name:match("%=$") then
+        option = option .. " "
+      end
+      table.insert(arr, option)
+    end
+  elseif type(shortOptions) == "string" then
+    for ch in shortOptions:gmatch(".") do
+      table.insert(arr, "-" .. ch)
+    end
+  end
+  return populateMatching(result, prefix, arr)
+end
+
 return {
   hasPrefix = hasPrefix,
   populateMatching = populateMatching,
+  populateOptions = populateOptions,
 }
