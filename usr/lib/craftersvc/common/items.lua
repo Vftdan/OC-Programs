68c00b8fbe9ddab093ecae0297394be5cfd78001
Vftdan <vftdan@gmail.com>
2026-02-05 13:05:35 +0100

    Recursively match item fields

--- usr/lib/craftersvc/common/items.lua
+++ usr/lib/craftersvc/common/items.lua
@@ -1,10 +1,37 @@
-local function matches(stack, specifiers)
-	for k, v in pairs(specifiers) do
-		if type(v) ~= "table" and stack[k] ~= v then
-			return false
-		end
+local function deepMatch(value, pattern, visitedMulti)
+	if pattern == nil then
+		return true
 	end
-	return true
+	if type(pattern) == "function" then
+		return pattern(value)
+	end
+	if type(value) ~= type(pattern) then
+		return false
+	end
+	if type(pattern) == "table" then
+		local visited = visitedMulti[pattern]
+		if not visited then
+			visited = {}
+			visitedMulti[pattern] = visited
+		end
+		if visited[value] then
+			return true
+		end
+		visited[value] = true
+		for k, v in pairs(pattern) do
+			if not deepMatch(value[k], v) then
+				return false
+			end
+		end
+		return true
+	else
+		return value == pattern
+	end
+end
+
+local function matches(stack, specifiers)
+	assert(type(specifiers) == "table")
+	return deepMatch(stack, specifiers, {})
 end
 
 return {
